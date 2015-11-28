6d45d5c479bfa334596be68506f97cb307845d2c
Mike Moss <mrmoss@alaska.edu>
2015-11-28 14:29:41 -0900

    Run script now accepts a user to run server as...

--- notify_server/run.sh
+++ notify_server/run.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-python -u notify_server.py >> /var/log/notify_server
+if [ $# -lt 1 ]
+then
+	echo "Usage: ./run.sh user_to_run_as"
+	exit 1
+fi
+
+sudo -u "$1" python -u notify_server.py >> /var/log/notify_server
