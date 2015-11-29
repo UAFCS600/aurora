d6bffd10006a5357a2565718682b8cf5ec30d05d
Mike Moss <mrmoss@alaska.edu>
2015-11-28 15:23:09 -0900

    Typo...

--- notify_server/notification_db.sql
+++ notify_server/notification_db.sql
@@ -3,7 +3,7 @@ drop table if exists clients;
 create table clients
 (
 	id integer auto_increment not null,
-	service token text not null,
+	service text not null,
 	token text not null,
 	kpTrigger integer not null,
 	primary key (id)
