d58bded58ab7dee122dab15db0719955854187a5
Dustin L. Dodson <dustinleedodson@gmail.com>
2016-02-07 22:26:55 -0900

    Added more information gathering from GPS

--- ionic/www/js/app.js
+++ ionic/www/js/app.js
@@ -33,9 +33,14 @@ function onDeviceReady() {
 
 function initGeoLocation() {
   navigator.geolocation.getCurrentPosition(function(position) {
-    alert("Latitude: " + position.coords.latitude +  '\n' +
-          "Longitude: " + position.coords.longitude + '\n' +
-          "Accuracy: " + position.coords.accuracy);
+    alert('Latitude: '          + position.coords.latitude          + '\n' +
+          'Longitude: '         + position.coords.longitude         + '\n' +
+          'Altitude: '          + position.coords.altitude          + '\n' +
+          'Accuracy: '          + position.coords.accuracy          + '\n' +
+          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+          'Heading: '           + position.coords.heading           + '\n' +
+          'Speed: '             + position.coords.speed             + '\n' +
+          'Timestamp: '         + position.timestamp                + '\n');
   },function(error) {
     alert("Could not get location!");
   });
