50082d8915dfe725f13780942c0164a49864b997
alexander lease <alexander lease>
2016-02-15 22:50:02 -0900

    Prettify

--- ionic/www/js/app.js
+++ ionic/www/js/app.js
@@ -1,164 +1,134 @@
-	// Ionic Starter App
-
-	// angular.module is a global place for creating, registering and retrieving Angular modules
-	// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-	// the 2nd parameter is an array of 'requires'
-	// 'starter.services' is found in services.js
-	// 'starter.controllers' is found in controllers.js
-	angular.module('aurora', ['ionic', 'aurora.controllers', 'aurora.services'])
-
-	.run(function($ionicPlatform) {
-		$ionicPlatform.ready(function() {
-			if (window.cordova && window.cordova.plugins.Keyboard) {
-				// Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-				// for form inputs)
-				cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-
-				// Don't remove this line unless you know what you are doing. It stops the viewport
-				// from snapping when text inputs are focused. Ionic handles this internally for
-				// a much nicer keyboard experience.
-				cordova.plugins.Keyboard.disableScroll(true);
-			}
-			if (window.StatusBar) {
-				StatusBar.styleDefault();
-			}
-			document.addEventListener("deviceready", onDeviceReady, false);
-			navigator.splashscreen.hide();
-		});	
-	})
-	
-	.config(function($stateProvider, $urlRouterProvider) {
-		// Ionic uses AngularUI Router which uses the concept of states
-		// Learn more here: https://github.com/angular-ui/ui-router
-		// Set up the various states which the app can be in.
-		// Each state's controller can be found in controllers.js
-		$stateProvider
-
-		// setup an abstract state for the tabs directive
-		.state('tab', {
-			url: '/tab',
-			abstract: true,
-			templateUrl: 'templates/tabs.html'
-		})
-
-		// Each tab has its own nav history stack:
-		.state('tab.dash', {
-			url: '/dash',
-			views: {
-				'tab-dash': {
-					templateUrl: 'templates/tab-dash.html',
-					controller: 'DashCtrl'
-				}
-			}
-		})
-	  
-		.state('tab.settings', {
-			url: '/settings',
-			views: {
-				'tab-settings': {
-					templateUrl: 'templates/tab-settings.html',
-					controller: 'SettingsCtrl'
-				}
-			}
-		})
-
-		.state('tab.location', {
-			url: '/location',
-			views: {
-				'tab-settings': {
-					templateUrl: 'templates/setting-location.html',
-				}
-			}
-		})
-		
-		.state('tab.kpAlert', {
-			url: '/kpAlert',
-			views: {
-				'tab-settings': {
-					templateUrl: 'templates/setting-kpa.html',
-				}
-			}
-		})
-		
-		.state('tab.allskyAlert', {
-			url: '/allskyAlert',
-			views: {
-				'tab-settings': {
-					templateUrl: 'templates/setting-allsky.html',
-				}
-			}
-		})
-		
-		.state('tab.quietTime', {
-			url: '/quietTime',
-			views: {
-				'tab-settings': {
-					templateUrl: 'templates/setting-quiet.html',
-				}
-			}
-		})
-		
-		.state('tab.tutorial', {
-			url: '/tutorial',
-			views: {
-				'tab-settings': {
-					templateUrl: 'templates/setting-tutorial.html',
-				}
-			}
-		})
-		
-		.state('tab.about', {
-			url: '/about',
-			views: {
-				'tab-about': {
-					templateUrl: 'templates/tab-about.html',
-					controller: 'AboutCtrl'
-				}
-			}
-		})
-		
-		.state('tab.allsky', {
-			url: '/allsky',
-			views: {
-				'tab-allsky': {
-					templateUrl: 'templates/tab-allsky.html',
-					controller: 'AllskyCtrl'
-				}
-			}
-		});
-
-		// if none of the above states are matched, use this as the fallback
-		$urlRouterProvider.otherwise('/tab/dash');
-	});
-
-	
-=======
 // Ionic Starter App
+
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+// 'starter.services' is found in services.js
+// 'starter.controllers' is found in controllers.js
+angular.module('aurora', ['ionic', 'aurora.controllers', 'aurora.services'])
 
 .run(function($ionicPlatform) {
-    $ionicPlatform.ready(function() {
-        //For some reason this block causes errors commented out for now -Dain
-		/*if (window.cordova && window.cordova.plugins.Keyboard) {
-            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-            // for form inputs)
-            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+	$ionicPlatform.ready(function() {
+		if (window.cordova && window.cordova.plugins.Keyboard) {
+			// Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+			// for form inputs)
+			cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 
-            // Don't remove this line unless you know what you are doing. It stops the viewport
-            // from snapping when text inputs are focused. Ionic handles this internally for
-            // a much nicer keyboard experience.
-            cordova.plugins.Keyboard.disableScroll(true);
-        }*/
-        if (window.StatusBar) {
-            StatusBar.styleDefault();
-        }
+			// Don't remove this line unless you know what you are doing. It stops the viewport
+			// from snapping when text inputs are focused. Ionic handles this internally for
+			// a much nicer keyboard experience.
+			cordova.plugins.Keyboard.disableScroll(true);
+		}
+		if (window.StatusBar) {
+			StatusBar.styleDefault();
+		}
+		document.addEventListener("deviceready", onDeviceReady, false);
+		navigator.splashscreen.hide();
+	});	
+})
 
-        document.addEventListener("deviceready", onDeviceReady, false);
-        navigator.splashscreen.hide();
-    });
+.config(function($stateProvider, $urlRouterProvider) {
+	// Ionic uses AngularUI Router which uses the concept of states
+	// Learn more here: https://github.com/angular-ui/ui-router
+	// Set up the various states which the app can be in.
+	// Each state's controller can be found in controllers.js
+	$stateProvider
+
+	// setup an abstract state for the tabs directive
+	.state('tab', {
+		url: '/tab',
+		abstract: true,
+		templateUrl: 'templates/tabs.html'
+	})
+
+	// Each tab has its own nav history stack:
+	.state('tab.dash', {
+		url: '/dash',
+		views: {
+			'tab-dash': {
+				templateUrl: 'templates/tab-dash.html',
+				controller: 'DashCtrl'
+			}
+		}
+	})
+  
+	.state('tab.settings', {
+		url: '/settings',
+		views: {
+			'tab-settings': {
+				templateUrl: 'templates/tab-settings.html',
+				controller: 'SettingsCtrl'
+			}
+		}
+	})
+
+	.state('tab.location', {
+		url: '/location',
+		views: {
+			'tab-settings': {
+				templateUrl: 'templates/setting-location.html',
+			}
+		}
+	})
+	
+	.state('tab.kpAlert', {
+		url: '/kpAlert',
+		views: {
+			'tab-settings': {
+				templateUrl: 'templates/setting-kpa.html',
+			}
+		}
+	})
+	
+	.state('tab.allskyAlert', {
+		url: '/allskyAlert',
+		views: {
+			'tab-settings': {
+				templateUrl: 'templates/setting-allsky.html',
+			}
+		}
+	})
+	
+	.state('tab.quietTime', {
+		url: '/quietTime',
+		views: {
+			'tab-settings': {
+				templateUrl: 'templates/setting-quiet.html',
+			}
+		}
+	})
+	
+	.state('tab.tutorial', {
+		url: '/tutorial',
+		views: {
+			'tab-settings': {
+				templateUrl: 'templates/setting-tutorial.html',
+			}
+		}
+	})
+	
+	.state('tab.about', {
+		url: '/about',
+		views: {
+			'tab-about': {
+				templateUrl: 'templates/tab-about.html',
+				controller: 'AboutCtrl'
+			}
+		}
+	})
+	
+	.state('tab.allsky', {
+		url: '/allsky',
+		views: {
+			'tab-allsky': {
+				templateUrl: 'templates/tab-allsky.html',
+				controller: 'AllskyCtrl'
+			}
+		}
+	});
+
+	// if none of the above states are matched, use this as the fallback
+	$urlRouterProvider.otherwise('/tab/dash');
 });
 
 function postToPushServer(params, onSuccess, onFailure) {
@@ -193,7 +163,6 @@ function requestTestPushNotification() {
     });
 }
 
->>>>>>> origin/test
 function onDeviceReady() {
     showGeoLocationInfo();
     initPushNotifications();
@@ -291,6 +260,3 @@ function initPushNotifications() {
         xhttp.send(JSON.stringify(params));
     }
 }
-=======
-}
->>>>>>> origin/test
