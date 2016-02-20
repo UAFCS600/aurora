7d8d4dc212b1052f39d2435ae425069b3d6171a6
Dain Harmon <dain.harmon@gmail.com>
2016-02-20 13:50:50 -0900

    Re-commented out the Keyboard code in app.js as it fails and prevents further code from running

--- ionic/www/js/app.js
+++ ionic/www/js/app.js
@@ -9,7 +9,8 @@ angular.module('aurora', ['ionic', 'aurora.controllers', 'aurora.services'])
 
 .run(function($ionicPlatform) {
 	$ionicPlatform.ready(function() {
-		if (window.cordova && window.cordova.plugins.Keyboard) {
+		//Keyboard code is dated and causes errors do not restore -Dain Harmon
+		/*if (window.cordova && window.cordova.plugins.Keyboard) {
 			// Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
 			// for form inputs)
 			cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
@@ -18,7 +19,7 @@ angular.module('aurora', ['ionic', 'aurora.controllers', 'aurora.services'])
 			// from snapping when text inputs are focused. Ionic handles this internally for
 			// a much nicer keyboard experience.
 			cordova.plugins.Keyboard.disableScroll(true);
-		}
+		}*/
 		if (window.StatusBar) {
 			StatusBar.styleDefault();
 		}
