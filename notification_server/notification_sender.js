52325e1e256651a7a3a83266227dff01996d0225
Dustin L. Dodson <dustinleedodson@gmail.com>
2016-04-22 18:37:49 -0800

    Can now potentially send GCM notifications.

--- notification_server/notification_sender.js
+++ notification_server/notification_sender.js
@@ -1,6 +1,91 @@
-var http = require('http');
+var http   = require('http');
+var gcm    = require('node-gcm');
+var config = require('./config.json');
+require('./db_util.js');
 
-requestOptions = {
+var getClientTokens = function(kpTrigger, service, onSuccess, onFailure) {
+    var db_util = new dbUtil();
+    db_util.getTokens(kpTrigger, service, onSuccess, onFailure);
+};
+
+var removeBadClients = function(clients, onSuccess, onFailure) {
+    var db_util = new dbUtil();
+    var success = false;
+
+    clients.forEach(function(client, index, array) {
+        db_util.removeClient({token:client},
+                             function(msg) {
+                                console.log('Successfully removed ' + client);
+                             },
+                             function(err) {
+                                console.log('Could not remove ' + client + ': ' + err);
+                             });
+    });
+}
+
+var getClientChunks = function(clientArr, chunkSize) {
+    var retArr = [];
+
+    var i, j, tempArray;
+
+    for (i = 0,j = clientArr.length; i < j; i += chunkSize) {
+        tempArray = clientArr.slice(i,i+chunkSize);
+        retArr.push(tempArray);
+    }
+    
+    return retArr;
+};
+
+var sendGCMMessages = function(kp, clients) {
+    console.log('Sending message to GCM clients: ' + clients);
+
+    var message = new gcm.Message();
+     
+    message.addData('message', '');
+     
+    var sender = new gcm.Sender(config.gcmApiKey);
+     
+    sender.send(message, { registrationTokens: clients }, function (err, response) {
+        if(err) console.error(err);
+        else {
+            var results    = response.results;
+            var badClients = [];
+            for(var i in results) {
+                if(results[i].error) {
+                    badClients.push(clients[i]);
+                }
+            }
+
+            removeBadClients(badClients,
+                             function() {console.log('Removed all bad clients');},
+                             function() {console.log('Could not remove all bad clients');});
+        }
+    });
+};
+
+var sendAPNSMessages = function(kp, clients) {
+    console.log('Sending message to APNS clients: ' + clients);
+};
+
+var sendKpToClients = function(kp) {
+    //Send GCM messages
+    getClientTokens(kp, 'gcm', function(tokens) {
+        var clientChunks = getClientChunks(tokens, 1000);
+        for(var i in clientChunks) sendGCMMessages(kp, clientChunks[i]);
+    }, function(err) {
+        console.log('Could not send GCM message: ' + err);
+    });
+
+    //Send APNS messages
+    getClientTokens(kp, 'apns', function(tokens) {
+        var clientChunks = getClientChunks(tokens, 1000);
+        for(var i in clientChunks) sendAPNSMessages(kp, clientChunks[i]);
+    }, function(err) {
+        console.log('Could not send APNS message: ' + err);
+    });
+};
+
+var requestOptions = {
 	host: 'cs472.gi.alaska.edu',
     path: '/kp.php?d=d'
 };
@@ -12,8 +97,10 @@ var receivedResponse = function(response) {
         body += d;
     });
     response.on('end', function() {
-
-        console.log(body);
+        var data      = JSON.parse(body);
+        var currentKp = Math.ceil(data.data[0].kp);
+        
+        sendKpToClients(currentKp);
     });
 };
 
