b263a36e81654f8d96e6aa37569e0148663b137b
Dustin L. Dodson <dustinleedodson@gmail.com>
2016-04-22 21:40:18 -0800

    Fixed formatting of time and query for getting tokens.

--- notification_server/db_util.js
+++ notification_server/db_util.js
@@ -115,9 +115,9 @@ dbUtil.prototype.getTokens = function(kpTrigger, service, onSuccess, onFailure) 
 		var minutes = today.getMinutes();
 		var seconds = today.getSeconds();
 
-		if(hours.length < 2)   hours   = '0' + hours;
-		if(minutes.length < 2) minutes = '0' + minutes;
-		if(seconds.length < 2) seconds = '0' + seconds;
+		if(hours < 10)   hours   = '0' + hours;
+		if(minutes < 10) minutes = '0' + minutes;
+		if(seconds < 10) seconds = '0' + seconds;
 
 		var time = hours + ':' +
 				   minutes + ':' +
@@ -135,7 +135,7 @@ dbUtil.prototype.getTokens = function(kpTrigger, service, onSuccess, onFailure) 
 		else {
 			var timeRightNow = getTimeNow();
 			var query = "SELECT token FROM clients WHERE notify_start_time < '" + timeRightNow +
-			            "' AND notify_stop_time > '" + timeRightNow + "' AND kpTrigger <= '" + 
+			            "' AND notify_stop_time > '" + timeRightNow + "' AND kpTrigger >= '" + 
 			            kpTrigger + "' AND service = '" + service + "'";
 		    console.log('Query: ' + query);
 
